8fba9a35f447b963ec59ff247c5e8e51c6c6ec4c
wcz0 <wcz0@outlook.com>
2021-04-26 11:43:51 +0800

    Revert "add"
    This reverts commit eea1d4dd4bf398b2f9013646343ba942f88a711a.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
-    return 'test';
+
+
+Route::get('/', function(){
+    return view('welcome');
 });
+
 Route::post('register', [RegisterController::class, 'register']);
