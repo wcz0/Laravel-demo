eea1d4dd4bf398b2f9013646343ba942f88a711a
wcz0 <wcz0@outlook.com>
2021-04-26 11:29:54 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/', function(){
-    return view('welcome');
+Route::get('/test', function () {
+    return 'test';
 });
-
 Route::post('register', [RegisterController::class, 'register']);
